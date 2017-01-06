44c12958972ead210eb70c07438452ef59c967d9
Martin Riedl <martin.riedl.92@googlemail.com>
2017-01-06 16:55:01 +0100

    run fist unit test on travis-ci

---
+++ ftp_test.go
@@ -0,0 +1,29 @@
+package goftp
+
+import "testing"
+
+func TestSimpleUpload(t *testing.T) {
+	// connect to remote server
+	ftp, err := NewFtp("localhost:21")
+	if err != nil {
+		t.Error("connection failed!", err)
+	}
+
+	// send user credentials
+	err = ftp.Login("test", "test")
+	if err != nil {
+		t.Error("logon failed", err)
+	}
+
+	// open remote directory
+	err = ftp.OpenDirectory("/test/")
+	if err != nil {
+		t.Error("change directory failed!", err)
+	}
+
+	// upload a local file to the remote FTP server
+	err = ftp.Upload("README.md", "README.md")
+	if err != nil {
+		t.Error("file upload failed!", err)
+	}
+}
